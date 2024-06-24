999076fa56634bc6ef4485938727b09c18a9176f
Choinhet <choinhet@gmail.com>
2024-06-24 09:43:25 -0300

    add git and tmux configs

---
+++ lua/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<cr>", {})
+            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<cr>", {})
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+}
