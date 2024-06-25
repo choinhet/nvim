3685048ceff5d8b9e0d294554b59c3d54b073620
Choinhet <choinhet@gmail.com>
2024-06-25 19:17:37 -0300

    wip

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,44 +1,48 @@
 return {
-    {
-        "neoclide/coc.nvim",
-        branch = "release",
-        config = function()
-            vim.g.coc_global_extensions = {
-                "coc-pyright",
-                "coc-json",
-                "coc-html",
-                "coc-css",
-                "coc-tsserver",
-            }
+	{
+		"neoclide/coc.nvim",
+		branch = "release",
+		config = function()
+			vim.g.coc_global_extensions = {
+				"coc-pyright",
+				"coc-json",
+				"coc-html",
+				"coc-css",
+				"coc-tsserver",
+			}
 
-            function _G.show_documentation()
-                local filetype = vim.bo.filetype
-                if vim.fn.index({"vim", "help"}, filetype) >= 0 then
-                    vim.api.nvim_command("h " .. vim.fn.expand("<cword>"))
-                elseif vim.fn["coc#rpc#ready"]() then
-                    vim.fn.CocActionAsync("doHover")
-                else
-                    vim.api.nvim_command("!" .. vim.bo.keywordprg .. " " .. vim.fn.expand("<cword>"))
-                end
-            end
+			function _G.show_documentation()
+				local filetype = vim.bo.filetype
+				if vim.fn.index({ "vim", "help" }, filetype) >= 0 then
+					vim.api.nvim_command("h " .. vim.fn.expand("<cword>"))
+				elseif vim.fn["coc#rpc#ready"]() then
+					vim.fn.CocActionAsync("doHover")
+				else
+					vim.api.nvim_command("!" .. vim.bo.keywordprg .. " " .. vim.fn.expand("<cword>"))
+				end
+			end
 
-            vim.api.nvim_set_keymap("n", "K", "<cmd>lua _G.show_documentation()<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "ge", "<Plug>(coc-diagnostic-next)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "gE", "<Plug>(coc-diagnostic-prev)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "gu", "<Plug>(coc-references)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "<space>ca", "<Plug>(coc-codeaction)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap(
-                "n",
-                "<leader>fm",
-                "<cmd>CocCommand editor.action.formatDocument<CR>",
-                { noremap = true, silent = true }
-            )
-        end,
-    },
-    {
-        "fannheyward/coc-pyright",
-    },
+			function _G.map_if_coc_ready(mode, lhs, rhs, opts)
+				opts = opts or { noremap = true, silent = true }
+				if vim.fn["coc#rpc#ready"]() then
+					vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+				end
+			end
+
+			local opts = { noremap = true, silent = true }
+
+			map_if_coc_ready("n", "K", "<cmd>lua _G.show_documentation()<CR>", opts)
+			map_if_coc_ready("n", "gd", "<Plug>(coc-definition)", opts)
+			map_if_coc_ready("n", "ge", "<Plug>(coc-diagnostic-next)", opts)
+			map_if_coc_ready("n", "gE", "<Plug>(coc-diagnostic-prev)", opts)
+			map_if_coc_ready("n", "gu", "<Plug>(coc-references)", opts)
+			map_if_coc_ready("n", "gi", "<Plug>(coc-implementation)", opts)
+			map_if_coc_ready("n", "<space>ca", "<Plug>(coc-codeaction)", opts)
+			map_if_coc_ready("n", "<leader>rn", "<Plug>(coc-rename)", opts)
+			map_if_coc_ready("n", "<leader>fm", "<cmd>CocCommand editor.action.formatDocument<CR>", opts)
+		end,
+	},
+	{
+		"fannheyward/coc-pyright",
+	},
 }
--- lua/plugins/virtual-environment.lua
+++ lua/plugins/virtual-environment.lua
@@ -1,14 +1,52 @@
-return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    config = function()
-        require("venv-selector").setup({})
-    end,
-    branch="regexp",
-    event = "VeryLazy",
-    keys = {
-        { "<leader>vs", "<cmd>VenvSelect<cr>" },
-        { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
-}
+local function update_python_path()
+	local venv_selector = require("venv-selector")
+	local venv_path = venv_selector.python()
 
+	if not venv_path then
+		print("venv_path was empty")
+		return
+	end
+
+	local settings = {}
+	settings["python.pythonPath"] = venv_path .. "/bin/python"
+
+	local settings_path = vim.fn.stdpath("config") .. "/coc-settings.json"
+	local file = io.open(settings_path, "w")
+
+	print("Loading completions for path:" .. venv_path)
+	if file then
+		file:write(vim.fn.json_encode(settings))
+		file:close()
+	end
+end
+
+local function on_venv_activate()
+	vim.defer_fn(function()
+		print("Updating completions source")
+		update_python_path()
+		vim.cmd("CocRestart")
+	end, 1000)
+end
+
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python",
+	},
+	branch = "regexp",
+	event = "VeryLazy",
+	config = function()
+		require("venv-selector").setup({
+            settings = {
+                options = {
+                    on_venv_activate_callback = on_venv_activate
+                }
+            }
+        })
+
+		vim.keymap.set("n", "<leader>vs", "<cmd>VenvSelect<cr>", {})
+	end,
+}
