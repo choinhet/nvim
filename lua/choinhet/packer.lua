e5cc09971e1e3892da166019cad270c7dc1c0577
rafael.choinhet <choinhet@gmail.com>
2025-01-13 16:29:46 -0300

    fix: file formatting

--- lua/choinhet/packer.lua
+++ lua/choinhet/packer.lua
@@ -1,28 +1,28 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use({
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end
-	})
+    use({
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    })
 
-	use({'nvim-treesitter/nvim-treesitter'}, { run = ':TSUpdate' })
+    use({'nvim-treesitter/nvim-treesitter'}, { run = ':TSUpdate' })
     use({'mg979/vim-visual-multi'}, {branch = 'master'})
-	use({'theprimeagen/harpoon'})
-	use({'tpope/vim-surround'})
-	use({'williamboman/mason.nvim'})
-	use({'williamboman/mason-lspconfig.nvim'})
-	use({'neovim/nvim-lspconfig'})
-	use({'hrsh7th/nvim-cmp'})
-	use({'hrsh7th/cmp-nvim-lsp'})
-	use({'rafamadriz/friendly-snippets'})
+    use({'theprimeagen/harpoon'})
+    use({'tpope/vim-surround'})
+    use({'williamboman/mason.nvim'})
+    use({'williamboman/mason-lspconfig.nvim'})
+    use({'neovim/nvim-lspconfig'})
+    use({'hrsh7th/nvim-cmp'})
+    use({'hrsh7th/cmp-nvim-lsp'})
+    use({'rafamadriz/friendly-snippets'})
 end)
