1ec7379f6f2467b5b207a5c9e676856ced7ba985
rafael.choinhet <choinhet@gmail.com>
2025-01-24 15:56:51 -0300

    feat: set oil to show hidden files

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,5 +1,8 @@
 require("oil").setup({
     vim.api.nvim_set_keymap('n', '<leader>pv', ':Oil<CR>', { noremap = true, silent = true }),
     vim.api.nvim_set_keymap('n', '-', ':Oil<CR>', { noremap = true, silent = true }),
+    view_options = {
+        show_hidden = true,
+    }
 })
 
